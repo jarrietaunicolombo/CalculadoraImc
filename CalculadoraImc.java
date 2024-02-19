aa843c3891cd86e7db200e6005b5f61864ecc3db
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2024-02-19 07:56:08 -0500

    Titulo del ejercicios

---
+++ CalculadoraImc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CalculadoraImc
+{
+    public static void main(String args[]){
+        System.out.println("========================");
+        System.out.println("*** CALCULO DEL IMC ****");
+        System.out.println("========================");
+        System.out.println(""); 
+    }
+
+}
