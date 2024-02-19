a0a67a5a386b19b2d9819c3225c842de076141d5
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2024-02-19 08:18:12 -0500

    Funcion que calcyula el IMC

--- CalculadoraImc.java
+++ CalculadoraImc.java
@@ -7,6 +7,22 @@ public class CalculadoraImc
         System.out.println("*** CALCULO DEL IMC ****");
         System.out.println("========================");
         System.out.println(""); 
+
+        float imc = calcularImc();
+        System.out.println("Su IMC es: "+imc);
+    }
+
+    public static float calcularImc(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite su peso en kg: ");
+        float peso = teclado.nextFloat();
+        System.out.print("Digite su altura en mts: ");
+        float altura = teclado.nextFloat();
+        if(peso <= 0 || altura <= 0){
+            System.out.println("El peso y la altura deben ser mayores a 0");
+            return 0;
+        }
+        return peso / (altura * altura);
     }
 
 }
